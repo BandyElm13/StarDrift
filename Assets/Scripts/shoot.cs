dba57e8c33c11720e9e47ce3c566303bc3f4c70e
BandyElm1313 <tyler13@colostate.edu>
2026-04-27 01:00:34 -0600

    Added Ammo
    1. Gon is no longer single shot. It has 30 bullets before you need to reload

--- Assets/Scripts/shoot.cs
+++ Assets/Scripts/shoot.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
 public class shoot : MonoBehaviour
 {
@@ -6,26 +6,61 @@ public class shoot : MonoBehaviour
     [SerializeField] private Transform firePoint;
     [SerializeField] private float bulletSpeed = 3f;
 
-    private float shootDelay = 3.5f;
+    private float shootDelay = 0.4f;
     private float shootTime = 0f;
-
+    
     [SerializeField] private AudioSource shootAudio;
     [SerializeField] private AudioSource reloadAudio;
 
+    [SerializeField] private TextMeshProUGUI ammountAmount;
+    private int totalBullets = 15;
+    private int currentBullets;
+    private bool isReloading = false;
+    private float reloadTime = 3.5f;
+    private float reloadTimer = 0f;
 
+    void Start()
+    {
+        currentBullets = totalBullets;
+        UpdateAmmoUI();
+    }
 
     void Update()
     {
         shootTime -= Time.deltaTime;
+        if(isReloading)
+        {
+            reloadTimer -= Time.deltaTime;
+            if(reloadTimer <= 0f)
+            {
+                currentBullets = totalBullets;
+                isReloading = false;
+                UpdateAmmoUI();
+            }
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.R) && currentBullets <= totalBullets)
+        {
+            startReload();
+            return;
+        }
+
         if(Input.GetMouseButtonDown(0))
         {
             if(shootTime <= 0f)
             {
+                if(currentBullets <= 0f)
+                {
+                    startReload();
+                    return;
+                }
                 Fire();
+                currentBullets--;
+                UpdateAmmoUI();
                 shootTime = shootDelay;
+                
                 shootAudio.timeSamples = (int)(0.2f * shootAudio.clip.frequency);
                 shootAudio.Play();
-                reloadAudio.PlayDelayed(shootAudio.clip.length);
             }
         }
     }
@@ -37,6 +72,21 @@ public class shoot : MonoBehaviour
         if (rb != null)
         {
             rb.linearVelocity = firePoint.forward * bulletSpeed;
+        }
+    }
+
+    private void startReload()
+    {
+        isReloading = true;
+        reloadTimer = reloadTime;
+        reloadAudio.Play();
+    }
+
+    private void UpdateAmmoUI()
+    {
+        if(ammountAmount != null)
+        {
+            ammountAmount.text = currentBullets + " / " + totalBullets;
         }
     }
 }
