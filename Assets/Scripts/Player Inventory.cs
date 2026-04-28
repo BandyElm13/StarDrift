0d6109213dc68fab4b0a34e5b79ab89cf215842b
BandyElm1313 <tyler13@colostate.edu>
2026-04-27 23:52:14 -0600

    i7ukyhfv 7

--- Assets/Scripts/Player Inventory.cs
+++ Assets/Scripts/Player Inventory.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI cointext;
-    public static int T_Coins;
+    public static int T_Coins = 19;
 
     public static HashSet<string> collectedCoins = new HashSet<string>();
 
